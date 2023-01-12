d168e595e73233bf03ade5f3365c2e538b0c5820
coder-yu <coderyy@outlook.com>
2023-01-13 03:14:16 +1000

    fix a small bug in ranking

--- util/algorithm.py
+++ util/algorithm.py
@@ -148,8 +148,8 @@ def find_k_largest(K, candidates):
     n_candidates.sort(key=lambda d: d[1], reverse=True)
     k_largest_scores = [item[1] for item in n_candidates]
     ids = [item[0] for item in n_candidates]
-    # find the K biggest scores
-    for iid, score in enumerate(candidates):
+    # find the K largest scores
+    for iid, score in enumerate(candidates[K:]):
         ind = K
         l = 0
         r = K - 1
@@ -169,5 +169,5 @@ def find_k_largest(K, candidates):
             ids[ind + 2:] = ids[ind + 1:-1]
         if ind < K - 1:
             k_largest_scores[ind + 1] = score
-            ids[ind + 1] = iid
+            ids[ind + 1] = iid+K
     return ids, k_largest_scores
